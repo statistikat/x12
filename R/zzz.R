4e48d7065157cd6cc4009a4ec5fc45752e575d6c
alexkowa <alex@kowarik.net>
2017-02-22 12:08:03 +0200

    remove x13path argument

--- R/zzz.R
+++ R/zzz.R
@@ -2,8 +2,9 @@
   #data(AirPassengers)
   packageStartupMessage("x12 is ready to use.")
   packageStartupMessage("Use the package x12GUI for a Graphical User Interface. \n")
-  packageStartupMessage("It is advised to set the path to the X12 or X13 executables\n")
-  packageStartupMessage("with x12path(validpath) or x13path(validpath)!\n")
+  x12path()
+  packageStartupMessage("By default the X13-ARIMA-SEATS binaries provided by\n")
+  packageStartupMessage("R package x13binary but this can be changed with x12path(validpath) \n")
   packageStartupMessage("---------------\n")
   packageStartupMessage("Suggestions and bug-reports can be submitted at: https://github.com/alexkowa/x12/issues")
 }
