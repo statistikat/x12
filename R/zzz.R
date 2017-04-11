2d8a9305cfea1886cb0fa125f26925700d038c02
Alexander Kowarik <alex@kowarik.net>
2017-04-11 10:03:56 +0200

    fix typo in startup message and link to issue tracking

--- R/zzz.R
+++ R/zzz.R
@@ -3,8 +3,8 @@
   packageStartupMessage("x12 is ready to use.")
   packageStartupMessage("Use the package x12GUI for a Graphical User Interface. \n")
   x12path()
-  packageStartupMessage("By default the X13-ARIMA-SEATS binaries provided by\n")
-  packageStartupMessage("R package x13binary but this can be changed with x12path(validpath) \n")
+  packageStartupMessage("By default the X13-ARIMA-SEATS binaries provided by the R package x13binary\n")
+  packageStartupMessage("are used but this can be changed with x12path(validpath) \n")
   packageStartupMessage("---------------\n")
-  packageStartupMessage("Suggestions and bug-reports can be submitted at: https://github.com/alexkowa/x12/issues")
+  packageStartupMessage("Suggestions and bug-reports can be submitted at: https://github.com/statistikat/x12/issues")
 }
